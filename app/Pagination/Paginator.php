e8bcb0c6841c2b492014eb4aaa7897a9b2f0a976
Javier Portillo <javiermarquezportillo@gmail.com>
2016-09-30 14:39:32 -0600

    Mueve la funcionalidad de paginación a su propia clase

---
+++ app/Pagination/Paginator.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Sigmalibre\Pagination;
+
+/**
+ * Calcula la cantidad de páginas según cuantos productos se quieran en cada página
+ * y según la cantidad de objetos totales que haya.
+ */
+class Paginator
+{
+    private $userInput;
+    private $itemsPerPage;
+    private $defaultItemsPerPage = 10;
+    private $maxItemsPerPage = 50;
+
+    /**
+     * Recibe directamente el input del usuario.
+     *
+     * @param array $userInput El input del usuario, este debe contener los parametros itemsPerPage y currentPage
+     */
+    public function __construct($userInput)
+    {
+        $this->userInput = $userInput;
+    }
+
+    /**
+     * Configura parametros para la validación de los items por página.
+     *
+     * @param int $default Valor por defecto para la cantidad de items por página, se usa  si la validación falla
+     * @param int $max     Valor máximo de items por página, se usa si el input es mayor a este valor
+     */
+    public function items($default, $max)
+    {
+        $this->defaultItemsPerPage = $default;
+        $this->maxItemsPerPage = $max;
+    }
+
+    public function calculate($itemCount)
+    {
+        // Verificar si el input de items por página existe y es válido, sino, por defecto es $this->defaultItemsPerPage;
+        $this->itemsPerPage = isset($this->userInput['itemsPerPage']) ? $this->userInput['itemsPerPage'] : $this->defaultItemsPerPage;
+
+        // Validar los items por página.
+        $this->itemsPerPage = $this->validate($this->itemsPerPage, $this->maxItemsPerPage, $this->defaultItemsPerPage);
+
+        // Calcular la cantidad total de páginas.
+        $totalPages = (int) ceil($itemCount / $this->itemsPerPage);
+
+        // Verificar si el input de la pagina actual existe y es válido, sino, por defecto es 1.
+        $currentPage = isset($this->userInput['currentPage']) ? $this->userInput['currentPage'] : 1;
+
+        // Validar la página actual.
+        $currentPage = $this->validate($currentPage, $totalPages, 1);
+
+        // Calcula cuantos items se omiten al inicio de la lista de resultados, creando un punto de inicio para la búsqueda.
+        $offset = ($currentPage - 1) * $this->itemsPerPage;
+
+        return [
+            'offset' => $offset,
+            'itemsPerPage' => $this->itemsPerPage,
+            'currentPage' => $currentPage,
+            'totalPages' => $totalPages,
+        ];
+    }
+
+    /**
+     * Valida el los input para la paginación de listas de resultado.
+     *
+     * @param string $input   Input desde el cliente
+     * @param int    $max     Cantidad máxima para el input
+     * @param int    $default Cantidad por defecto si el input es incorrecto
+     * @param int    $min     Cantidad mínima para el input
+     *
+     * @return int El input validado
+     */
+    private function validate($input, $max, $default)
+    {
+        if (is_numeric($input) === false) {
+            return $default;
+        }
+
+        $input = (int) $input;
+
+        if ($input < 1) {
+            return 1;
+        }
+
+        if ($input > $max) {
+            return $max;
+        }
+
+        return $input;
+    }
+}
--- app/Products/Products.php
+++ app/Products/Products.php
@@ -17,7 +17,7 @@ class Products
     }
 
     /**
-     * Instancia las fuentes de datos para hacer una lectura de la lista de productos.
+     * Lee la lista de todos los productos, sin filtrar resultados, con capacidad de paginación.
      *
      * @return array Lista de los productos
      */
@@ -25,67 +25,25 @@ class Products
     {
         $productSearch = new ProductSearch(new DataSource\MySQL\CountAllProducts($this->container));
 
-        // Validar los productos por página.
-        $productsPerPage = isset($this->userInput['productsPerPage']) ? $this->userInput['productsPerPage'] : 10;
-        $productsPerPage = $this->validatePagination($productsPerPage, 50, 10, 1);
-
         // Realizar un conteo de todos los productos existentes.
         $rowCount = (int) $productSearch->search([])[0]['cuenta'];
 
-        // Calcular la cantidad de páginas según cuantos productos se quieran ver.
-        $totalPages = (int) ceil($rowCount / $productsPerPage);
-
-        // Validar la página que el cliente quiere ver.
-        $productsPage = isset($this->userInput['productsPage']) ? $this->userInput['productsPage'] : 1;
-        $productsPage = $this->validatePagination($productsPage, $totalPages, 1, 1);
-
-        $pagination = [
-            'currentPage' => $productsPage,
-            'perPage' => $productsPerPage,
-        ];
+        // Obtiene detalles para la paginación.
+        $paginator = new \Sigmalibre\Pagination\Paginator($this->userInput);
+        $pagination = $paginator->calculate($rowCount);
 
         $productSearch->setStrategy(new DataSource\MySQL\SearchAllProducts($this->container));
 
         // Retornar la búsqueda de los productos según la paginación.
-        $searchResults = $productSearch->search($pagination);
+        $searchResults = $productSearch->search([
+            'offset' => $pagination['offset'],
+            'items' => $pagination['itemsPerPage'],
+        ]);
 
         return [
             'productList' => $searchResults,
-            'pagination' => [
-                'currentPage' => $productsPage,
-                'perPage' => $productsPerPage,
-                'totalPages' => $totalPages,
-            ],
+            'pagination' => $pagination,
             'userInput' => $this->userInput,
         ];
-    }
-
-    /**
-     * Valida el los input para la paginación de la lista de productos.
-     *
-     * @param string $input   Input desde el cliente
-     * @param int    $max     Cantidad máxima para el input
-     * @param int    $default Cantidad por defecto si el input es incorrecto
-     * @param int    $min     Cantidad mínima para el input
-     *
-     * @return int El input validado
-     */
-    private function validatePagination($input, $max, $default, $min = 1)
-    {
-        if (is_numeric($input) === false) {
-            return $default;
-        }
-
-        $input = (int) $input;
-
-        if ($input < $min) {
-            return $min;
-        }
-
-        if ($input > $max) {
-            return $max;
-        }
-
-        return $input;
     }
 }
