2c184fcc06d1f47e647c388379df6a683a37a8de
Javier Portillo <javiermarquezportillo@gmail.com>
2016-10-01 22:00:36 -0600

    Agrega vista para la página de búsqueda de categorías

--- app/routes.php
+++ app/routes.php
@@ -3,3 +3,5 @@
 $app->get('/', '\Sigmalibre\Homepage\HomeController:home')->setName('homepage');
 
 $app->get('/productos', '\Sigmalibre\Products\ProductsController:indexProducts')->setName('products');
+
+$app->get('/categorias', '\Sigmalibre\Categories\CategoriesController:indexCategories')->setName('categories');
