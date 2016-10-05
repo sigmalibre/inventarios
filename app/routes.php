1d17cbde4b263d9de0ac988006230f45b2b69577
Javier Portillo <javiermarquezportillo@gmail.com>
2016-10-05 16:58:34 -0600

    Agrega facturas a las rutas

--- app/routes.php
+++ app/routes.php
@@ -7,3 +7,5 @@ $app->get('/productos', '\Sigmalibre\Products\ProductsController:indexProducts')
 $app->get('/categorias', '\Sigmalibre\Categories\CategoriesController:indexCategories')->setName('categories');
 
 $app->get('/proveedores', '\Sigmalibre\Providers\ProvidersController:indexProviders')->setName('providers');
+
+$app->get('/facturas', '\Sigmalibre\Invoices\InvoicesController:indexInvoices')->setName('invoices');
