5bcce04513afdce342638bb5661fb8788c48f31a
Javier Portillo <javiermarquezportillo@gmail.com>
2016-10-04 21:09:18 -0600

    Agrega los proveedores a las rutas

--- app/routes.php
+++ app/routes.php
@@ -5,3 +5,5 @@ $app->get('/', '\Sigmalibre\Homepage\HomeController:home')->setName('homepage');
 $app->get('/productos', '\Sigmalibre\Products\ProductsController:indexProducts')->setName('products');
 
 $app->get('/categorias', '\Sigmalibre\Categories\CategoriesController:indexCategories')->setName('categories');
+
+$app->get('/proveedores', '\Sigmalibre\Providers\ProvidersController:indexProviders')->setName('providers');
