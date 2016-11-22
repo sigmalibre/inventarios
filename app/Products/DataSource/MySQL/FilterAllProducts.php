4f1c4aaf4c2d6fe5771d96bf412c71e671bef819
Javier Portillo <javiermarquezportillo@gmail.com>
2016-11-22 05:43:08 -0600

    Cambia el query de búsqueda de productos para el nuevo diseño

--- app/Products/DataSource/MySQL/FilterAllProducts.php
+++ app/Products/DataSource/MySQL/FilterAllProducts.php
@@ -7,7 +7,7 @@ namespace Sigmalibre\Products\DataSource\MySQL;
  */
 class FilterAllProducts extends \Sigmalibre\DataSource\MySQL\MySQLReader
 {
-    protected $baseQuery = 'SELECT ProductoID, Productos.Codigo as CodigoProducto, Descripcion, ExcentoIVA, StockMin, PrecioVenta, FechaCreacion, FechaModificacion, UnidadMedida, CategoriaProductoID, CategoriaProductos.Codigo as CodigoCategoria, CategoriaProductos.Nombre as NombreCategoria, MarcaID, Marcas.Nombre as NombreMarca, COALESCE(SUM(DetalleIngresos.Cantidad), 0) - COALESCE(SUM(DetalleFactura.Cantidad), 0) as Cantidad FROM Productos LEFT JOIN CategoriaProductos USING (CategoriaProductoID) LEFT JOIN Marcas USING (MarcaID) LEFT JOIN DetalleIngresos USING (ProductoID) LEFT JOIN DetalleFactura USING (ProductoID) WHERE 1';
+    protected $baseQuery = 'SELECT ProductoID, Productos.Codigo as CodigoProducto, Descripcion, ExcentoIVA, StockMin, PrecioVenta, FechaCreacion, FechaModificacion, UnidadMedida, CategoriaProductoID, CategoriaProductos.Codigo as CodigoCategoria, CategoriaProductos.Nombre as NombreCategoria, MarcaID, Marcas.Nombre as NombreMarca, COALESCE(SUM(DetalleIngresos.Cantidad), 0) - COALESCE(SUM(DetalleFactura.Cantidad), 0) as Cantidad FROM Productos LEFT JOIN CategoriaProductos USING (CategoriaProductoID) LEFT JOIN Marcas USING (MarcaID) LEFT JOIN DetalleIngresos USING (ProductoID) LEFT JOIN DetalleFactura USING (ProductoID) LEFT JOIN Medidas USING (MedidaID) WHERE 1';
     protected $setLimit = true;
     protected $filterFields = [
         [
