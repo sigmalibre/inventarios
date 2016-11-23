1568b45f63e8450b644b5fd9f36d007b21e4f864
Javier Portillo <javiermarquezportillo@gmail.com>
2016-11-23 04:09:01 -0600

    Arregla bug de MySQLReader
    Arregla bug donde se crea conflicto en los parametros de búsqueda si en el query hay dos columnas con el mismo nombre.

--- app/DataSource/MySQL/MySQLReader.php
+++ app/DataSource/MySQL/MySQLReader.php
@@ -54,20 +54,20 @@ abstract class MySQLReader implements \Sigmalibre\DataSource\ReadDataSourceInter
 
                 // Si el filtro es de tipo LIKE. Ej: SELECT * FROM tabla WHERE columna LIKE 'termino de búsqueda%'
                 if ($filter['searchType'] === 'LIKE') {
-                    $this->filters[] = "{$filter['tableName']}.{$filter['columnName']} LIKE :{$filter['columnName']}";
-                    $this->params[$filter['columnName']] = $input[$filter['filterName']].'%';
+                    $this->filters[] = "{$filter['tableName']}.{$filter['columnName']} LIKE :{$filter['filterName']}";
+                    $this->params[$filter['filterName']] = $input[$filter['filterName']].'%';
                 }
 
                 // Si el filtro es de tipo FULL TEXT. Ej: SELECT * FROM tabla WHERE MATCH(columna) AGAINST('termino de búsqueda')
                 if ($filter['searchType'] === 'MATCH') {
-                    $this->filters[] = "MATCH({$filter['tableName']}.{$filter['columnName']}) AGAINST(:{$filter['columnName']})";
-                    $this->params[$filter['columnName']] = $input[$filter['filterName']];
+                    $this->filters[] = "MATCH({$filter['tableName']}.{$filter['columnName']}) AGAINST(:{$filter['filterName']})";
+                    $this->params[$filter['filterName']] = $input[$filter['filterName']];
                 }
 
                 // Si el filtro es de tipo igualdad. Ej: SELECT * FROM tabla WHERE columna = 'termino de búsqueda'
                 if ($filter['searchType'] === '=') {
-                    $this->filters[] = "{$filter['tableName']}.{$filter['columnName']} = :{$filter['columnName']}";
-                    $this->params[$filter['columnName']] = $input[$filter['filterName']];
+                    $this->filters[] = "{$filter['tableName']}.{$filter['columnName']} = :{$filter['filterName']}";
+                    $this->params[$filter['filterName']] = $input[$filter['filterName']];
                 }
             }
         }
