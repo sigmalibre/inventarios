53e4d7a6ae5d2d38eb85f0377e347705d2ee0729
Javier Portillo <javiermarquezportillo@gmail.com>
2016-12-02 19:29:15 -0600

    Arregla bug del formulario de nuevo producto
    Al seleccionar una opción del select de categoría por defecto, no se aplica el código de la categoría al input del código del producto.

--- public/js/new-product-set-cat-code.js
+++ public/js/new-product-set-cat-code.js
@@ -3,11 +3,17 @@
 $(function () {
     'use strict';
 
-    $('body').on('change', '.new-product-category-select', function (event) {
-        if (event.currentTarget.value.length === 0) {
+    var setValue = function () {
+        if ($('.new-product-category-select').val().length === 0) {
             $('.new-product-cat-preview').text('--');
             return;
         }
-        $('.new-product-cat-preview').text(event.currentTarget.value);
+        $('.new-product-cat-preview').text($('.new-product-category-select').val());
+    };
+
+    $('body').on('change', '.new-product-category-select', function () {
+        setValue();
     });
+
+    setValue();
 });
