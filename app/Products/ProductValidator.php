442a7b02402e924cce4b443825ce97f6f51f7228
Javier Portillo <javiermarquezportillo@gmail.com>
2016-12-04 17:05:47 -0600

    Añade validación para campos de nuevo producto

---
+++ app/Products/ProductValidator.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Sigmalibre\Products;
+
+/**
+ * Maneja la validación de datos para las distintas acciones sobre productos.
+ */
+class ProductValidator
+{
+    private $container;
+    private $invalidUserInputs = [];
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Si uno de los métodos de guardado de datos retorna falso, y es culpa de un input
+     * que no pasó la validación, se puede usar esta función para obtener una lista con
+     * los inputs que no pasaron la prueba, por ejemplo para darle feedback al usuario.
+     *
+     * @return array La lista de los inputs inválidos
+     */
+    public function getInvalidInputs()
+    {
+        return $this->invalidUserInputs;
+    }
+
+    /**
+     * Ajustar manualmente un input como inválido.
+     *
+     * Nesesario en situaciones donde se hace validación extra afuera de esta clase.
+     *
+     * @param string $inputName Nombre del input
+     */
+    public function setInvalidInput($inputName)
+    {
+        $this->invalidUserInputs[$inputName] = true;
+    }
+
+    /**
+     * Realiza las validaciones específicas para crear un nuevo producto.
+     *
+     * @param array $userInput Input del usuario a validar
+     *
+     * @return bool True si ha pasado las validaciones; False de lo contrario
+     */
+    public function validateNewProduct($userInput)
+    {
+        $validator = $this->container->validator;
+
+        // El código del producto debe ser un string sin espacios de 1 a 20 caracteres de largo.
+        if ($validator::stringType()->noWhitespace()->length(1, 20)->validate($userInput['codigoProducto']) === false) {
+            $this->invalidUserInputs['codigoProducto'] = true;
+
+            return false;
+        }
+
+        // La descripción del producto debe ser un string de 1 a 30 caracteres.
+        if ($validator::stringType()->length(1, 30)->validate($userInput['descripcionProducto']) === false) {
+            $this->invalidUserInputs['descripcionProducto'] = true;
+
+            return false;
+        }
+
+        // El producto es excento de IVA, valor opcional ya que por defecto los productos están sin excepción al IVA.
+        // Debe ser un valor booleano.
+        if ($validator::optional($validator::boolVal())->validate($userInput['excentoIvaProducto']) === false) {
+            $this->invalidUserInputs['excentoIvaProducto'] = true;
+
+            return false;
+        }
+
+        // El stock mínimo del producto, esta cantidad se utiliza para enviar una alerta cuando el producto está por terminarse.
+        // Debe ser un valor numérico entero positivo incluyendo el cero.
+        if ($validator::numeric()->intVal()->min(0, true)->validate($userInput['stockMinProducto']) === false) {
+            $this->invalidUserInputs['stockMinProducto'] = true;
+
+            return false;
+        }
+
+        // El precio de venta del producto debe ser un número positivo, incluyendo el cero.
+        if ($validator::numeric()->min(0, true)->validate($userInput['precioVentaProducto']) === false) {
+            $this->invalidUserInputs['precioVentaProducto'] = true;
+
+            return false;
+        }
+
+        // El código de la referencia del libro DET, debe ser un string de 2 caracteres.
+        if ($validator::stringType()->noWhitespace()->length(2, 2)->validate($userInput['referenciaLibroDetProducto']) === false) {
+            $this->invalidUserInputs['referenciaLibroDetProducto'] = true;
+
+            return false;
+        }
+
+        // El código del bien DET debe ser un string sin espacios de 2 dígitos de largo.
+        if ($validator::stringType()->noWhitespace()->length(2, 2)->validate($userInput['categoriaDetProducto']) === false) {
+            $this->invalidUserInputs['categoriaDetProducto'] = true;
+
+            return false;
+        }
+
+        // La marca del producto debe ser un string de 100 caracteres o menos
+        if ($validator::stringType()->length(1, 100)->validate($userInput['marcaProducto']) === false) {
+            $this->invalidUserInputs['marcaProducto'] = true;
+
+            return false;
+        }
+
+        // La medida del producto debe ser un string de 100 caracteres o menos
+        if ($validator::stringType()->length(1, 100)->validate($userInput['medidaProducto']) === false) {
+            $this->invalidUserInputs['medidaProducto'] = true;
+
+            return false;
+        }
+
+        // El código de la categoría debe ser un string sin espacios de 2 dígitos de largo.
+        if ($validator::stringType()->noWhitespace()->length(2, 2)->validate($userInput['categoriaProducto']) === false) {
+            $this->invalidUserInputs['categoriaProducto'] = true;
+
+            return false;
+        }
+
+        return true;
+    }
+}
