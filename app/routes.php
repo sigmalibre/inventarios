ad2f98376b76941c42fb810454d349651b838393
Javier Portillo <javiermarquezportillo@gmail.com>
2016-12-15 11:27:43 -0600

    Añade comentarios al archivo de rutas

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,9 @@
 <?php
 
+// HOMEPAGE
 $app->get('/', '\Sigmalibre\Homepage\HomeController:home')->setName('homepage');
 
+// PRODUCTOS
 $app->get('/productos', '\Sigmalibre\Products\ProductsController:indexProducts')->setName('products');
 $app->post('/productos', '\Sigmalibre\Products\ProductsController:createNew');
 
@@ -10,22 +12,29 @@ $app->post('/productos/id/{id}', '\Sigmalibre\Products\ProductsController:update
 
 $app->get('/productos/createform', '\Sigmalibre\Products\ProductsController:indexNewProduct')->setName('products/createform');
 
+// CATEGORIAS DE PRODUCTO
 $app->get('/categorias', '\Sigmalibre\Categories\CategoriesController:indexCategories')->setName('categories');
 $app->post('/categorias', '\Sigmalibre\Categories\CategoriesController:createNew');
 
 $app->get('/categorias/createform', '\Sigmalibre\Categories\CategoriesController:indexNewCategory')->setName('categories/createform');
 
+// PROVEEDORES
 $app->get('/proveedores', '\Sigmalibre\Providers\ProvidersController:indexProviders')->setName('providers');
 
+// FACTURACIÓN
 $app->get('/facturas', '\Sigmalibre\Invoices\InvoicesController:indexInvoices')->setName('invoices');
 
 $app->get('/creditofiscal', '\Sigmalibre\Invoices\InvoicesController:indexCreditoFiscal')->setName('creditofiscal');
 
+// ALMACENES
 $app->get('/warehouses', '\Sigmalibre\Warehouses\WarehousesController:indexWarehouses')->setName('warehouses');
 
+// CLIENTES
 $app->get('/clientes/personas', '\Sigmalibre\Clients\ClientsController:indexPeople')->setName('clientes/personas');
 $app->get('/clientes/empresas', '\Sigmalibre\Clients\ClientsController:indexCompanies')->setName('clientes/empresas');
 
+// MARCAS
 $app->get('/marcas', '\Sigmalibre\Brands\BrandsController:indexBrands')->setName('brands');
 
+// UNIDADES DE MEDIDA
 $app->get('/medidas', '\Sigmalibre\UnitsOfMeasurement\UnitsOfMeasurementController:indexMeasurements')->setName('measurements');
