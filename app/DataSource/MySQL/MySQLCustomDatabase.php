d99859a5bd8692606d506ad457cbd01edeca12d4
Javier Portillo <javiermarquezportillo@gmail.com>
2016-12-24 21:01:06 -0600

    Añade conexión a BD MySQL personalizada

---
+++ app/DataSource/MySQL/MySQLCustomDatabase.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Sigmalibre\DataSource\MySQL;
+
+/**
+ * Crea conexiones a una BD especificada.
+ */
+class MySQLCustomDatabase extends MySQL
+{
+    public function __construct($database)
+    {
+        $dsn = 'mysql:dbname='.$database.';host=localhost;charset=utf8mb4';
+
+        $this->connection = new \PDO($dsn, 'invuser', 'invpass');
+        $this->connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_SILENT);
+        $this->connection->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+        $this->connection->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+    }
+}
