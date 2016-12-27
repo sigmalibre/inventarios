e29f0a9e34fa7a8484d0e4a3cb63fe2898e4188d
Javier Portillo <javiermarquezportillo@gmail.com>
2016-12-27 00:07:42 -0600

    Añade soporte para transacciones anidadas

--- app/DataSource/MySQL/MySQLTransactions.php
+++ app/DataSource/MySQL/MySQLTransactions.php
@@ -10,6 +10,14 @@ namespace Sigmalibre\DataSource\MySQL;
 class MySQLTransactions extends MySQL
 {
     /**
+     * Cuenta la cantidad de transacciones iniciadas, para evitar transacciones
+     * anidadas.
+     *
+     * @var int
+     */
+    protected $transactionCounter = 0;
+
+    /**
      * Inicia una transacción.
      *
      * Por defecto, MySQL no soporta transacciones anidadas, debido a esto,
@@ -20,9 +28,29 @@ class MySQLTransactions extends MySQL
      */
     public function beginTransaction()
     {
+        // Probar que no estemos dentro de una transacción.
         if ($this->connection->inTransaction() === false) {
-            return $this->connection->beginTransaction();
+
+            // Si no estamos dentro de una transacción, pero ya se han iniciado
+            // varias, significa que en algun momento falló la conexión o
+            // ocurrió algún error y nos salimos de la transacción.
+            if ($this->transactionCounter > 0) {
+                throw new \RuntimeException('No se encuentra transacción existente');
+            }
+
+            // Probamos si se inició la transacción.
+            if ($this->connection->beginTransaction() === true) {
+                $this->transactionCounter = 1;
+
+                return true;
+            }
+
+            // Si no se inició la transacción.
+            throw new \RuntimeException('No se pudo iniciar una transacción');
         }
+
+        // Si ya estamos dentro de una transacción.
+        ++$this->transactionCounter;
 
         return true;
     }
@@ -45,10 +73,42 @@ class MySQLTransactions extends MySQL
     public function commit()
     {
         if ($this->connection->inTransaction() === false) {
+
+            // Si no estamos dentro de una transacción, pero ya se han iniciado
+            // varias, significa que en algun momento falló la conexión o
+            // ocurrió algún error y nos salimos de la transacción.
+            if ($this->transactionCounter > 0) {
+                throw new \RuntimeException('No se encuentra transacción existente');
+            }
+
             return false;
         }
 
-        return $this->connection->commit();
+        // Si estamos dentro de una transacción.
+
+        // Si esa transacción no ha sido iniciada en este script.
+        if ($this->transactionCounter < 1) {
+            throw new \RuntimeException('Ya existe una transacción iniciada por otro servicio');
+        }
+
+        // Si hay transacciones pseudo-anidadas, al hacer commit se reduce la
+        // cantidad por uno.
+        if ($this->transactionCounter > 1) {
+            --$this->transactionCounter;
+
+            return true;
+        }
+
+        // Si la cuenta de transacciones es una sola.
+        // Si se pudo realizar el commit.
+        if ($this->connection->commit() === true) {
+            $this->transactionCounter = 0;
+
+            return true;
+        }
+
+        // Si no se pudo realizar el commit.
+        throw new \RuntimeException('No se pudo cometer la transacción actual');
     }
 
     /**
@@ -59,9 +119,32 @@ class MySQLTransactions extends MySQL
     public function rollBack()
     {
         if ($this->connection->inTransaction() === false) {
+
+            // Si no estamos dentro de una transacción, pero ya se han iniciado
+            // varias, significa que en algun momento falló la conexión o
+            // ocurrió algún error y nos salimos de la transacción.
+            if ($this->transactionCounter > 0) {
+                throw new \RuntimeException('No se encuentra transacción existente');
+            }
+
             return false;
         }
 
-        return $this->connection->rollBack();
+        // Si estamos dentro de una transacción.
+
+        // Si esa transacción no ha sido iniciada en este script.
+        if ($this->transactionCounter < 1) {
+            throw new \RuntimeException('Ya existe una transacción iniciada por otro servicio');
+        }
+
+        // Si se pudo realizar el rollback.
+        if ($this->connection->rollBack() === true) {
+            $this->transactionCounter = 0;
+
+            return true;
+        }
+
+        // Si no se pudo realizar el commit.
+        throw new \RuntimeException('No se pudo revertir la transacción actual');
     }
 }
