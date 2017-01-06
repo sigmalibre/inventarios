fc41e55f8cfd27c3b8f6cdb8561887ba268e37fe
Javier Portillo <javiermarquezportillo@gmail.com>
2017-01-06 13:59:13 -0600

    Cambia modelo de almacén para actualizar mejor sus datos

--- app/Warehouses/Warehouse.php
+++ app/Warehouses/Warehouse.php
@@ -1,6 +1,13 @@
 <?php
 
 namespace Sigmalibre\Warehouses;
+
+use Sigmalibre\DatosGenerales\DataSource\MySQL\UpdateDireccionAlmacen;
+use Sigmalibre\DatosGenerales\DataSource\MySQL\UpdateTelefonoAlmacen;
+use Sigmalibre\DatosGenerales\Direccion;
+use Sigmalibre\DatosGenerales\Telefono;
+use Sigmalibre\Validation\ValidadorDireccion;
+use Sigmalibre\Validation\ValidadorTelefono;
 use Sigmalibre\Warehouses\DataSource\MySQL\GetWarehouseFromID;
 use Sigmalibre\Warehouses\DataSource\MySQL\UpdateWarehouse;
 
@@ -11,14 +18,61 @@ class Warehouse
 {
     private $container;
     private $validator;
-    private $attributes;
-    private $dataSource;
+    private $validadorDirecciones;
+    private $validadorTelefonos;
+
+    private $id;
+    private $nombre;
+    private $direccion;
+    private $telefono;
+    private $cantidad;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDireccion()
+    {
+        return $this->direccion;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCantidad()
+    {
+        return $this->cantidad;
+    }
 
     public function __construct($id, $container)
     {
         $this->container = $container;
         $this->validator = new WarehousesValidator();
-        $this->dataSource = new GetWarehouseFromID($container);
+        $this->validadorDirecciones = new ValidadorDireccion();
+        $this->validadorTelefonos = new ValidadorTelefono();
 
         $this->init($id);
     }
@@ -27,14 +81,29 @@ class Warehouse
      * Inicializa los datos de un almacén a partir de su ID.
      *
      * @param $id
+     *
+     * @return bool
      */
     public function init($id)
     {
-        $this->attributes = $this->dataSource->read([
+        $dataSource = new GetWarehouseFromID($this->container);
+        $attributes = $dataSource->read([
             'input' => [
                 'idAlmacen' => $id,
             ],
         ]);
+
+        if ($attributes === false) {
+            return false;
+        }
+
+        $this->id = $id;
+        $this->nombre = $attributes[0]['NombreAlmacen'];
+        $this->direccion = $attributes[0]['Direccion'];
+        $this->telefono = $attributes[0]['Telefono'];
+        $this->cantidad = $attributes[0]['Cantidad'];
+
+        return $this->is_set();
     }
 
     /**
@@ -44,54 +113,71 @@ class Warehouse
      */
     public function is_set()
     {
-        return isset($this->attributes[0]);
-    }
-
-    public function getID()
-    {
-        return $this->attributes[0]['AlmacenID'];
-    }
-
-    public function getNombre()
-    {
-        return $this->attributes[0]['NombreAlmacen'];
-    }
-
-    private function setNombre(string $nombre)
-    {
-        $this->attributes[0]['NombreAlmacen'] = $nombre;
+        // Garantizar que al menos se obtiene el nombre.
+        return isset($this->nombre);
     }
 
     /**
      * Actualiza la fuente de datos sobre la información de este objeto.
      *
-     * @param array $userInput
+     * @param array $input
      *
      * @return bool
      */
-    public function update(array $userInput)
+    public function update(array $input)
     {
         // Limpiar los espacios en blanco al inicio y final de todos los inputs.
-        $userInput = array_map('trim', $userInput);
+        $input = array_map('trim', $input);
 
         // Validar el input del usuario.
-        if ($this->validator->validate($userInput) === false) {
+        $isInputValid = $this->runValidators($input);
+        if ($isInputValid === false) {
             return false;
         }
 
-        $userInput['id'] = $this->getID();
+        $this->container->mysql->beginTransaction();
 
-        $writer = new UpdateWarehouse($this->container);
+        $isWarehouseUpdated = $this->updateWarehouse($input['nombreAlmacen']);
 
-        $isSaved = $writer->write($userInput);
+        if ($isWarehouseUpdated === false) {
+            $this->container->mysql->rollBack();
 
-        if ($isSaved === false) {
             return false;
         }
 
-        $this->setNombre($userInput['nombreAlmacen']);
+        $isDirectionUpdated = (new Direccion())->save(new UpdateDireccionAlmacen($this->container), $input['direccion'], ['almacenID' => $this->id]);
+        $isTelefonoUpdated = (new Telefono())->save(new UpdateTelefonoAlmacen($this->container), $input['telefono'], ['almacenID' => $this->id]);
+
+        if ($isDirectionUpdated === false || $isTelefonoUpdated === false) {
+            $this->container->mysql->rollBack();
+
+            return false;
+        }
+
+        $this->container->mysql->commit();
+
+        $this->init($this->id);
 
         return true;
+    }
+
+    private function updateWarehouse($name)
+    {
+        $writer = new UpdateWarehouse($this->container);
+
+        return $writer->write([
+            'nombreAlmacen' => $name,
+            'id' => $this->id,
+        ]);
+    }
+
+    public function runValidators(array $input)
+    {
+        $this->validator->validate($input);
+        $this->validadorDirecciones->validate($input);
+        $this->validadorTelefonos->validate($input);
+
+        return empty($this->getInvalidInputs());
     }
 
     /**
@@ -101,6 +187,10 @@ class Warehouse
      */
     public function getInvalidInputs()
     {
-        return $this->validator->getInvalidInputs();
+        return array_merge(
+            $this->validator->getInvalidInputs(),
+            $this->validadorDirecciones->getInvalidInputs(),
+            $this->validadorTelefonos->getInvalidInputs()
+        );
     }
 }
