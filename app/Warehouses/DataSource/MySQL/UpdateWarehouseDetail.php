c2406ff472dc17d39f4f76303af5a4dcf9df9c81
Javier Portillo <javiermarquezportillo@gmail.com>
2017-01-06 19:13:09 -0600

    Agrega fuente de datos para actualizar la cantidad de producto en un almacén

---
+++ app/Warehouses/DataSource/MySQL/UpdateWarehouseDetail.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Sigmalibre\Warehouses\DataSource\MySQL;
+
+use Sigmalibre\DataSource\MySQL\MySQLTransactions;
+
+/**
+ * Realiza una actualización al dato de la cantidad de producto existente
+ * en un almacén.
+ */
+class UpdateWarehouseDetail
+{
+    /** @var MySQLTransactions $connection */
+    private $connection;
+
+    public function __construct($container)
+    {
+        $this->connection = $container;
+    }
+
+    /**
+     * Actualiza la cantidad de producto existente en un almacén.
+     *
+     * Advertencia: los datos deberán llegar ya validados en este punto.
+     *
+     * @param $data
+     *
+     * @return bool
+     */
+    public function write($data)
+    {
+        return $this->connection->execute('UPDATE DetalleAlmacenes SET Cantidad = :cantidadIngreso WHERE AlmacenID = :almacenID AND ProductoID = :productoID', $data);
+    }
+}
