6a4f936a8e38e3f29c538f68e4bfc3b6abe81688
Javier Portillo <javiermarquezportillo@gmail.com>
2017-03-21 01:11:11 -0600

    Agrega middleware para probar si el usuario ha iniciado sesión
    Y si no ha iniciado sesión, se redirige al formulario de inicio de sesión.

---
+++ app/Accounts/LogIn/LoggedInCheckMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Sigmalibre\Accounts\LogIn;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class LoggedInCheckMiddleware
+{
+    public function __invoke(Request $request, Response $response, $next)
+    {
+        if ($request->getRequestTarget() === '/login') {
+            return $next($request, $response);
+        }
+
+        if (isset($_SESSION['uid']) === false) {
+            return $response->withRedirect('/login');
+        }
+
+        return $next($request, $response);
+    }
+}
