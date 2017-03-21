e4440452289a4e2e33f6e31d8d20c1229a95911f
Javier Portillo <javiermarquezportillo@gmail.com>
2017-03-20 23:09:03 -0600

    Agrega controlador para mostrar reporte de conteo de inventario físico

--- app/Reports/ReporteController.php
+++ app/Reports/ReporteController.php
@@ -3,6 +3,7 @@
 namespace Sigmalibre\Reports;
 
 use Sigmalibre\DET\DETReport;
+use Sigmalibre\Reports\ReportBuilders\CorteProductosReportBuilder;
 use Sigmalibre\Reports\ReportBuilders\TestReportBuilder;
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -46,5 +47,26 @@ class ReporteController
             ->write($det->run())
             ->withHeader('Content-Disposition', 'attachment; filename="det.txt"')
             ->withHeader('Content-Type', 'text/plain');
+    }
+
+    public function conteoInventario(Request $request, Response $response)
+    {
+        $params = $request->getQueryParams();
+
+        if (empty($params['category']) === true) {
+            return $response->withRedirect('/reportes');
+        }
+
+        $builder = new ReporteBuilderDirector(new CorteProductosReportBuilder($this->container, $params['category']));
+
+        $reporte = $builder->make();
+
+        $reporteRenderizado = $this->loader->render($reporte);
+
+        return (new Response())
+            ->withHeader('Content-Type', 'application/pdf')
+            ->withHeader('Content-Disposition', 'inline ' . utf8_encode('reporte.pdf'))
+            ->withHeader('Cache-Control', 'private, max-age=0, must-revalidate')
+            ->write($reporteRenderizado);
     }
 }
