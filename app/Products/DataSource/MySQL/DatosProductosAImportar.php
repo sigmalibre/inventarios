fa43a3c75ab17f4790b73254056c2d7d64757c66
Javier Portillo <javiermarquezportillo@gmail.com>
2017-04-24 11:57:07 -0600

    De los datos del respaldo solo se ocupa la utilidad
    El resto viene del archivo de excel

--- app/Products/DataSource/MySQL/DatosProductosAImportar.php
+++ app/Products/DataSource/MySQL/DatosProductosAImportar.php
@@ -12,19 +12,8 @@ class DatosProductosAImportar extends MySQLReaderCustomConnection
     protected $baseQuery = '
     SELECT
         codigo_mas as Codigo,
-        nombre_cat as Categoria,
-        nombre_mas as Descripcion,
-        precio1_cst as Utilidad,
-        codigo_reflibrodet as ReferenciaLibroDet,
-        codigo_catbiendet as CodigoBienDet,
-        marca_mas as Marca,
-        nombre_medida as Medida,
-        saldou_mas as Unidades,
-        promedio_mas as Costo
+        precio1_cst as Utilidad
     FROM tbmaster
-    LEFT JOIN tbcategoriaproductos USING (codigo_cat)
-    LEFT JOIN tbcostos USING (codigo_mas)
-    LEFT JOIN tbmedida USING (codigo_medida)
-    WHERE activo = 1';
+    LEFT JOIN tbcostos USING (codigo_mas)';
     protected $setLimit = false;
 }
